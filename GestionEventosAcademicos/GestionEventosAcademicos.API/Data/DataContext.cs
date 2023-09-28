544d71b90950cb385eded760e358fffaedfd4827
Sebasgv13 <segave613@gmail.com>
2023-09-27 23:33:46 -0500

    COMMIT conexion a base de datos, creacion de entidad EventosAcademicos y su respectivo controlador.

---
+++ GestionEventosAcademicos/GestionEventosAcademicos.API/Data/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using GestionEventosAcademicos.Shared.Entities;
+
+
+namespace GestionEventosAcademicos.API.Data
+{
+    public class DataContext : DbContext
+    {
+        //Utilizar  las propiedades y opciones de DBContext
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+
+        }
+        public DbSet<EventoAcademico> evento_academico { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
